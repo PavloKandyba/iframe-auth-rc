2764dd628bc162094b33b4a6171d0b446ffe4824
Pavlo K <48647819+PavloKandyba@users.noreply.github.com>
2023-12-19 02:31:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,21 +7,18 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
-// CORS in case you need
+// CORS setup
 app.use((req, res, next) => {
-  res.set('Access-Control-Allow-Origin', 'https://Kandyba.rocket.chat'); // Rocket.Chat URL, adjust as needed
+  res.set('Access-Control-Allow-Origin', 'https://kandyba.rocket.chat');
   res.set('Access-Control-Allow-Credentials', 'true');
-
   next();
 });
 
-const baseURL = 'https://kandyba.rocket.chat'; // Rocket.Chat base URL, adjust as needed
-const yourPersonalAccessToken = 'AlTHWmACFLK1wEKIZv7cDy6UZvHQiKYykwIIE4GNmA6'; // Replace with your actual token
-const yourAdminUserID = '99jQmj4DPxsWeyL8v'; // Replace with your admin user ID
+const baseURL = 'https://kandyba.rocket.chat';
+const yourPersonalAccessToken = 'AlTHWmACFLK1wEKIZv7cDy6UZvHQiKYykwIIE4GNmA6';
+const yourAdminUserID = '99jQmj4DPxsWeyL8v';
 
 const generateRandomUsername = () => {
-  // This example uses base-36 conversion to limit username length to 10 characters
   return 'user' + Math.floor(Math.random() * 1000000000).toString(36).substring(0, 10);
 };
 
@@ -36,12 +33,10 @@ const generateRandomPassword = (length = 12) => {
 };
 
 const generateRandomEmail = () => {
-  // This example generates a temporary email address
   return `useremail${Math.floor(Math.random() * 1000000000)}@example.com`;
 };
 
 const generateRandomName = () => {
-  // This example creates a name with "Guest User" prefix and a random number
   return `name${Math.floor(Math.random() * 1000)}`;
 };
 
@@ -54,34 +49,21 @@ app.post('/sso', async (req, res) => {
     return res.sendStatus(401);
   }
 
-  // Attempt creating user (if needed)
   try {
     const username = generateRandomUsername();
     const password = generateRandomPassword();
     const email = generateRandomEmail();
     const name = generateRandomName();
 
+    // Create user
     await axios.post(
       `${baseURL}/api/v1/users.create`,
-      {
-        username,
-        password,
-        email,
-        name,
-      },
-      {
-        headers: {
-          'X-Auth-Token': yourPersonalAccessToken,
-          'X-User-Id': yourAdminUserID,
-        },
-      }
+      { username, password, email, name },
+      { headers: { 'X-Auth-Token': yourPersonalAccessToken, 'X-User-Id': yourAdminUserID } }
     );
 
     // Login with created credentials
-    const loginResponse = await axios.post(`${baseURL}/api/v1/login`, {
-      username,
-      password,
-    });
+    const loginResponse = await axios.post(`${baseURL}/api/v1/login`, { username, password });
 
     if (loginResponse.data.status === 'success') {
       const authToken = loginResponse.data.data.authToken;
